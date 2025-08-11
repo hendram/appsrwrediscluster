75d0965512f86afe0f3d0db704a6a54156749fe5
root <root@debian.localhost>
2025-08-11 11:19:11 +0000

    remove strict

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import { AuthProvider } from './AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <AuthProvider>
       <RwMain />
     </AuthProvider>
-  </React.StrictMode>
 );
